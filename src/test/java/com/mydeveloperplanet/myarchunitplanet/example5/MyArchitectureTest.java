8b67dbc19e45f97b355eb12e4aec6bfcb818df88
mydeveloperplanet <gunter@mydeveloperplanet.com>
2025-03-29 10:13:28 +0100

    Added failOnEmpty test

--- src/test/java/com/mydeveloperplanet/myarchunitplanet/example5/MyArchitectureTest.java
+++ src/test/java/com/mydeveloperplanet/myarchunitplanet/example5/MyArchitectureTest.java
@@ -40,9 +40,58 @@ class MyArchitectureTest {
                 .spring(spring -> spring
                         .noAutowiredFields()
                         .boot(boot -> boot
-                                .springBootApplicationShouldBeIn("com.enofex.taikai"))
+                                .springBootApplicationShouldBeIn("com.mydeveloperplanet.myarchunitplanet.example5"))
                         .configurations(configuration -> configuration
                                 .namesShouldEndWithConfiguration())
+                        .controllers(controllers -> controllers
+                                .shouldBeAnnotatedWithRestController()
+                                .namesShouldEndWithController()
+                                .shouldNotDependOnOtherControllers()
+                                .shouldBePackagePrivate())
+                        .services(services -> services
+                                .shouldBeAnnotatedWithService()
+                                .shouldNotDependOnControllers()
+                                .namesShouldEndWithService())
+                        .repositories(repositories -> repositories
+                                .shouldBeAnnotatedWithRepository()
+                                .shouldNotDependOnServices()
+                                .namesShouldEndWithRepository()))
+                .build()
+                .check();
+    }
+
+    @Test
+    void shouldFulfillConstraintsFailOnEmpty() {
+        Taikai.builder()
+                .namespace("com.mydeveloperplanet.myarchunitplanet.example5")
+                .failOnEmpty(true)
+                .java(java -> java
+                        .noUsageOfDeprecatedAPIs()
+                        .methodsShouldNotDeclareGenericExceptions()
+//                        .utilityClassesShouldBeFinalAndHavePrivateConstructor()
+                        .imports(imports -> imports
+                                .shouldHaveNoCycles()
+                                .shouldNotImport("..shaded..")
+                                .shouldNotImport("org.junit.."))
+                        .naming(naming -> naming
+                                .classesShouldNotMatch(".*Impl")
+                                .methodsShouldNotMatch("^(foo$|bar$).*")
+                                .fieldsShouldNotMatch(".*(List|Set|Map)$")
+//                                .fieldsShouldMatch("com.enofex.taikai.Matcher", "matcher")
+//                                .constantsShouldFollowConventions()
+                                .interfacesShouldNotHavePrefixI()))
+                .logging(logging -> logging
+                        .loggersShouldFollowConventions(Logger.class, "logger", List.of(PRIVATE, FINAL)))
+                .test(test -> test
+                        .junit5(junit5 -> junit5
+                                .classesShouldNotBeAnnotatedWithDisabled()
+                                .methodsShouldNotBeAnnotatedWithDisabled()))
+                .spring(spring -> spring
+                        .noAutowiredFields()
+//                        .boot(boot -> boot
+//                                .springBootApplicationShouldBeIn("com.mydeveloperplanet.myarchunitplanet.example5"))
+//                        .configurations(configuration -> configuration
+//                                .namesShouldEndWithConfiguration())
                         .controllers(controllers -> controllers
                                 .shouldBeAnnotatedWithRestController()
                                 .namesShouldEndWithController()
